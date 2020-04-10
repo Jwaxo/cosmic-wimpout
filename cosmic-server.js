1fcc43fee7763e1c787e9f8d0d8e170818445688
Jeff Wax <jeff@jeffwax.com>
2020-04-10 13:37:22 -0700

    Add board constructor and basics.

--- cosmic-server.js
+++ cosmic-server.js
@@ -13,6 +13,10 @@ const request = require('request');
 const chalk = require('chalk');
 const stripAnsi = require('strip-ansi');
 
+const Board = require('./src/factories/cosmic-boardfactory');
+
+const clients = [];
+const chatHistory = [];
 const app = express();
 const server = http.Server(app);
 const uuid = require('uuid/v4');
@@ -39,10 +43,73 @@ module.exports = (cosmic, config) => {
   Wimpout = cosmic;
   console_colors = config.get('server.console_colors');
 
+  // Currently we only run one board at a time, so create Wimpout with a board.
+  const board = Wimpout.getBoardById(1);
 
   // Listen at the port.
   server.listen(port, () => {
     console.log(`Listening on ${port}`);
   });
 
+  serverLog(`New game board generated with ID ${board.getId()}.`, true);
+
+  // Do basic server setup stuff.
+  app.use(express.static(__dirname + '/public'));
+
+  // Here we do the Express/IO shared session magic.
+  app.use(session);
+  io.use(sharedSession(session));
+
+  app.set("twig options", {
+    allow_async: true,
+    strict_variables: false
+  });
+
+  // Serve up the default page.
+  app.get('/', (req, res) => {
+    res.render('index.twig', {
+      board,
+      chatHistory,
+      recaptcha_key: config.get('recaptcha.key'),
+    });
+  });
 };
+
+/**
+ * Creates a simple message for displaying to the server, with timestamp.
+ *
+ * @param {string} message
+ *   The message to set. This can have chalk.js formatting, which will be stripped
+ *   prior to sending to clients.
+ * @param {boolean} serverOnly
+ *   If the message should not be broadcast in the clientside chat.
+ */
+function serverLog(message, serverOnly = false) {
+  const date = new Date();
+  const timestamp = date.toLocaleString("en-US");
+  const log = `${timestamp}: ${message}`;
+
+  if (!serverOnly) {
+    updateChat(log);
+  }
+
+  console.log(log);
+}
+
+/**
+ * Adds a new line item to the chat box.
+ *
+ * @param {string} message
+ *   The message to add. This is expected to be formatted using chalk.js for
+ *   console formatting; it will be stripped.
+ */
+function updateChat(message) {
+  // Define how console colors look so we can remove them from the HTML.
+  message = stripAnsi(message);
+
+  chatHistory.unshift(message);
+
+  Twig.renderFile('./views/chat-item.twig', {message}, (error, html) => {
+    io.sockets.emit('update-chat', html);
+  });
+}
--- src/factories/cosmic-wimpoutfactory.js
+++ src/factories/cosmic-wimpoutfactory.js
@@ -8,7 +8,7 @@
  */
 
 const Twig = require('twig');
-
+const Board = require('./cosmic-boardfactory');
 
 class Wimpout {
 
@@ -20,13 +20,73 @@ class Wimpout {
     this.config = config;
     this.system = {};
     this.players = {};
-    this.games = {};
+    this.last_player_id = 0;
+    this.boards = {};
+    this.last_board_id = 0;
+
+    // Ensure that our first board has an id of 1. Ideally we would just say
+    // this.addBoard(new Board(this));
+    this.addBoard(new Board(this), 1);
+  }
+
+  /**
+   * Adds a board to Wimpout's configuration.
+   *
+   * @param board
+   * @param board_id
+   *
+   * @return int
+   *   The board's ID, new or old.
+   */
+  addBoard(board, board_id = null) {
+    // If no ID is provided, we're either adding a loaded board to memory or we
+    // are creating a fresh board and need it to be assigned an ID.
+    if (board_id === null) {
+      let loaded_board_id = board.getId();
+
+      // If the board already has an ID, we slot it in with that ID.
+      if (loaded_board_id !== null) {
+        if (this.boards.hasOwnProperty(loaded_board_id)) {
+          throw "Tried to load a board that is already loaded.";
+        }
+        else {
+          this.boards[loaded_board_id] = board;
+        }
+      }
+      // Otherwise we create an ID for it and assign it.
+      else {
+        ++this.last_board_id;
+        this.boards[this.last_board_id] = board;
+        board.setId(this.last_board_id);
+      }
+    }
+    else {
+      if (this.boards[board_id]) {
+        throw "Tried to use a Board ID that already exists.";
+      }
+      else {
+        this.boards[board_id] = board;
+        board.setId(board_id);
+      }
+    }
+    return board.getId();
+  }
+
+  getBoardById(board_id) {
+    return this.boards[board_id];
   }
 
   async setupAll() {
+    // Currently nothing to setup, but ideally this is where any saved states
+    // would be loaded into memory.
     return Promise.all([
       // Run various setup functions.
+      this.setupBoards(),
     ]);
+  }
+
+  async setupBoards() {
+    return this.boards;
   }
 }
 
